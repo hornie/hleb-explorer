7f640b7ea6fdc53de107f570cb031e9d99010192
hornie <hornminer@gmail.com>
2019-04-01 20:13:09 +0300

    I guess we don't need strings for the api to work

--- q/util.php
+++ q/util.php
@@ -1,6 +1,6 @@
 <?php
 
-function build_post_context(string $postdata) {
+function build_post_context($postdata) {
         return stream_context_create(array(
                 'http' =>
                         array(
@@ -12,11 +12,11 @@ function build_post_context(string $postdata) {
         );
 };
 
-function build_rpc_body(string $method, string $params) {
+function build_rpc_body($method, $params) {
         return '{"jsonrpc":"2.0","id":"blockexplorer","method":"'.$method.'","params":'.$params.'}';
 };
 
-function fetch_rpc(string $api, string $method, string $params) {
+function fetch_rpc($api, $method, $params) {
         $url = $api . '/json_rpc';
         $rendered_rpc = build_rpc_body($method, $params);
         $context = build_post_context($rendered_rpc);
@@ -24,7 +24,7 @@ function fetch_rpc(string $api, string $method, string $params) {
         return json_decode($response, true);
 };
 
-function fetch_getinfo(string $api) {
+function fetch_getinfo($api) {
         $_url = $api . '/getinfo';
         $response = file_get_contents($_url);
         return json_decode($response, true);
