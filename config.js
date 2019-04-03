c76174db762cf793f1f4662a70e0ee527d487940
hornie <hornminer@gmail.com>
2019-04-03 11:42:48 +0300

    Added 'coins left to mine' thing. Also changed block target interval

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'http://82.146.56.17:18922';
 var donationAddress = "";
-var blockTargetInterval = 30; // enter the block interval in seconds
-var coinUnits = 1000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+var blockTargetInterval = 120; // enter the block interval in seconds
+var coinUnits = 1000;  // enter in the amount of atomic units in 1 coin, eg. 1000 crums = 1 HLEB
 var totalSupply =  120000000000000; // enter the total supply in atomic units
 var symbol = 'HLEB'; // enter the coin's ticker
 var refreshDelay = 30000;
