a8ae0310fb064d942ebb92c9eb6d5f2e997016ae
hornie <hornminer@gmail.com>
2019-04-06 01:29:07 +0300

    Put in an alt symbol param to make forking easier

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 1000;  // enter in the amount of atomic units in 1 coin, eg. 1000 crums = 1 HLEB
 var totalSupply =  120000000000000; // enter the total supply in atomic units
 var symbol = 'HLEB'; // enter the coin's ticker
+var symbolLong = 'HLEB'; //used in "Coins left to mine". It can either be the same as your "symbol", or a longer/alt version of it. aka XMR = Monero.
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
